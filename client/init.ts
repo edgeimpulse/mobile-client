f9627185977b839720d3e74ecfc70f2bdcda2a1d
Jenny Plunkett <jenny@edgeimpulse.com>
2021-08-03 12:37:28 -0500

    Remove data collection elements, show only classifier

--- client/init.ts
+++ client/init.ts
@@ -1,42 +1,17 @@
-import { DataCollectionClientViews } from "./collection-views";
 import { ClassificationClientViews } from "./classification-views";
 import { getIngestionApi, storeIngestionApi, getRemoteManagementEndpoint,
     storeRemoteManagementEndpoint, storeStudioEndpoint, getStudioEndpoint } from "./settings";
-import { CameraDataCollectionClientViews } from "./camera-collection-views";
-import { DataCollectionKeywordClientViews } from "./collection-keyword";
 import { MicrophoneDataCollectionClientViews } from "./microphone-collection-views";
 
 export default async function mobileClientLoader(mode:
-    'data-collection' | 'classifier' | 'data-collection-camera' | 'data-collection-keyword' |
-    'data-collection-microphone') {
+    'classifier' ) {
 
     storeIngestionApi(getIngestionApi());
     storeRemoteManagementEndpoint(getRemoteManagementEndpoint());
     storeStudioEndpoint(getStudioEndpoint());
 
-    if (mode === 'data-collection') {
-        let client = new DataCollectionClientViews();
-        await client.init();
-        (window as any).client = client;
-
-    }
-    else if (mode === 'classifier') {
+    if (mode === 'classifier') {
         let client = new ClassificationClientViews();
-        await client.init();
-        (window as any).client = client;
-    }
-    else if (mode === 'data-collection-camera') {
-        let client = new CameraDataCollectionClientViews();
-        await client.init();
-        (window as any).client = client;
-    }
-    else if (mode === 'data-collection-microphone') {
-        let client = new MicrophoneDataCollectionClientViews();
-        await client.init();
-        (window as any).client = client;
-    }
-    else if (mode === 'data-collection-keyword') {
-        let client = new DataCollectionKeywordClientViews();
         await client.init();
         (window as any).client = client;
     }
